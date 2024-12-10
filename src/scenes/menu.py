c74d270ed47d3f3d683ced4c4dda00dcc5fe8476
SilverHawks <thekeke60@gmail.com>
2024-12-10 19:19:18 +0100

    add sound and responsive fix

--- src/scenes/menu.py
+++ src/scenes/menu.py
@@ -4,10 +4,9 @@ import pygame
 class Menu:
     def __init__(self, screen):
         self.screen = screen
-        # Load the custom font
-        self.font = pygame.font.Font(os.path.join("assets", "fonts", "western.ttf"), 74)
         self.options = ["Start Game", "Options", "Quit"]
-        self.selected_option = 0
+        self.font = pygame.font.Font(os.path.join("assets", "fonts", "western.ttf"), 74)
+        self.selected_option = -1
 
         # Get the size of the screen
         self.screen_width, self.screen_height = self.screen.get_size()
@@ -16,39 +15,49 @@ class Menu:
         self.background = pygame.image.load(os.path.join("assets", "images", "ecran_titre.jpg"))
         self.background = pygame.transform.scale(self.background, (self.screen_width, self.screen_height))
 
+        # Load sounds
+        self.hover_sound = pygame.mixer.Sound(os.path.join("assets", "sounds", "hover_sound.mp3"))
+        self.bg_sound = pygame.mixer.Sound(os.path.join("assets", "sounds", "main_music.mp3"))
+
+        # Initialize audio channels
+        self.bg_channel = pygame.mixer.Channel(0)
+        self.hover_channel = pygame.mixer.Channel(1)
+
+        # Play background sound
+        self.bg_channel.set_volume(0.05)
+        self.bg_channel.play(self.bg_sound, loops=-1)
+
     def display_menu(self):
         self.screen.blit(self.background, (0, 0))
+        font_size = int(self.screen_height * 0.07)  # Calculate font size based on screen height
+        font = pygame.font.Font(os.path.join("assets", "fonts", "western.ttf"), font_size)
         for i, option in enumerate(self.options):
             if i == self.selected_option:
-                label = self.font.render(option, True, (250, 214, 57))
+                label = font.render(option, True, (250, 214, 57))
             else:
-                label = self.font.render(option, True, (255, 255, 255))
-            label_rect = label.get_rect(center=(self.screen_width*0.508, self.screen_height*0.22 + i * 85))
+                label = font.render(option, True, (255, 255, 255))
+            label_rect = label.get_rect(center=(self.screen_width*0.508, self.screen_height*0.22 + i * (font_size + 10)))
             self.screen.blit(label, label_rect)
         pygame.display.flip()
 
     def handle_event(self, event):
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_UP:
-                self.selected_option = (self.selected_option - 1) % len(self.options)
-            elif event.key == pygame.K_DOWN:
-                self.selected_option = (self.selected_option + 1) % len(self.options)
-            elif event.key == pygame.K_RETURN:
-                if self.selected_option == 0:
-                    return "start_game"
-                elif self.selected_option == 1:
-                    return "quit"
-        elif event.type == pygame.MOUSEMOTION:
+        if event.type == pygame.MOUSEMOTION:
             mouse_x, mouse_y = event.pos
+            hovered = False
             for i, option in enumerate(self.options):
                 label = self.font.render(option, True, (255, 255, 255))
-                label_rect = label.get_rect(center=(self.screen_width*0.508, self.screen_height*0.22 + i * 85))
+                label_rect = label.get_rect(center=(self.screen_width * 0.508, self.screen_height * 0.22 + i * 85))
                 if label_rect.collidepoint(mouse_x, mouse_y):
+                    if self.selected_option != i:
+                        self.hover_channel.play(self.hover_sound)
                     self.selected_option = i
+                    hovered = True
+            if not hovered:
+                self.selected_option = -1
         elif event.type == pygame.MOUSEBUTTONDOWN:
             if event.button == 1:  # Left mouse button
                 if self.selected_option == 0:
                     return "start_game"
-                elif self.selected_option == 1:
+                elif self.selected_option == 2:
                     return "quit"
         return None
