5a2b62182d139fa66a12b6cdb1455d349ebb9dcf
MMKay9 <mathuard95@gmail.com>
2024-12-11 13:05:20 +0100

    da change

---
+++ assets/images/sprite/titleset/Used/element_sable.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="element_sable" tilewidth="16" tileheight="16" tilecount="4000" columns="16">
+ <image source="../[Base]BaseChip_pipo.png" width="256" height="4000"/>
+</tileset>
---
+++ assets/images/sprite/titleset/Used/maison2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="maison" tilewidth="16" tileheight="16" tilecount="961" columns="31">
+ <image source="../../Documents/Ecole/Esiee Licence/Python/projet python/western_game/assets/images/sprite/titleset/title_set_house2.png" width="500" height="500"/>
+</tileset>
